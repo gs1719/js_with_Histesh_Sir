da6ad4b536685da792d9ace57a0927ad046d994f
Gaurav Srivastava <52217208+gs1719@users.noreply.github.com>
2025-07-09 16:52:09 +0000

    added all promises

--- basics/promise.js
+++ basics/promise.js
@@ -5,6 +5,7 @@
 //4. Object.create()
 //=========================================================
 //1.Object Literal
+
 // const person = {
 //     name:"Alice",
 //     age:30,
@@ -17,7 +18,7 @@
 // console.log(person.name);
 // person.greet();
 
-//-------------------------------
+
 //========================================
 // constructor function
 
@@ -26,7 +27,7 @@
 //     this.age = age;
 // }
 
-// //adding a method to the prototype to be shared by all instances
+// adding a method to the prototype to be shared by all instances
 // Person.prototype.greet = function(){
 //     console.log(`Hello, my name is ${this.name}`);
 // };
@@ -38,24 +39,146 @@
 // person1.greet();
 // person2.greet();
 
-//-----------------------------------------------
+
 //-----------------------------------------------
 //===============================================
 //creating the Promise
-const myPromise = new Promise((resolve,reject)=>{
-    setTimeout(()=>{const success = true;
+// const myPromise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         const success = true;
     
-        if(success){
-            resolve("Data fetched successfully!");//Fulill the promise
-        }
-        else{
-            reject(new Error("Failed to fetch data.")); //Reject the promise
-        }
+//         if(success){
+//             resolve("Data fetched successfully!");//Fulill the promise
+//         }
+//         else{
+//             reject(new Error("Failed to fetch data.")); //Reject the promise
+//         }
 
-    },2000); //runs after 2s
-});
+//     },2000); //runs after 2s
+// });
 
-//consuming the promise
-myPromise.then((message)=>{console.log(message);}).catch((error)=>{console.log(error.message);});
+//consuming the promise1
+//important thing to note that resolve is connected to then 
+//so anything thing passed from above will be catched here
 
-console.log("Proimse created, waiting for it to settle...");
+// we are doing chaining here there could be multiple then and one catch and one finally
+
+// myPromise
+//     .then((message)=>{console.log(message);})
+//     .catch((error)=>{console.log(error.message);});
+
+// console.log("Proimse created, waiting for it to settle...");
+
+
+//===============================================
+//===============================================
+
+// 1)
+// const promiseOne = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log(`Async task is completed`);
+//         resolve();
+//     },1000);
+// });
+
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// });
+
+
+// ======================================
+// ======================================
+// 2)
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("Async task 2"); 
+//         resolve();
+//     },1000);
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// });
+
+// ======================================
+// ======================================
+
+
+// 3)
+// const promiseThree = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve({username:"Gaurav",password:"password123",email:"gauravsriup@gmail.com"});
+//     },1000);
+// });
+
+// //consumption
+// promiseThree.then(function(user){console.log(user);}).catch().finally();
+
+
+// =======================================
+// =======================================
+//4)
+//creation
+
+// const promiseFour = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = true;
+//         if(error){
+//             resolve({username:"Gaurav",password:[{otp:1234,text:"password"}]});
+//         }
+//         else{
+//             reject('Error:something went wrong');
+//         }
+//     },1000);
+// });
+
+//consumption
+
+// promiseFour.then( (user)=>{
+//     console.log(user);
+//     return user.password[0];
+// }).then(
+//     (password)=>{
+//     console.log(password.text,password.otp);
+//     }
+// ).catch(
+//     function(error){
+//         console.log(error);
+//     }
+
+// ).finally(
+//     ()=>{console.log("Will be executed anyway");
+
+//     }
+// );
+
+
+// ===========================================
+// ============================================
+//5)
+// creation of promise
+// const promiseFive = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let error = false;
+//         if(!error){
+//             resolve({
+//                 username:"Gaurav",
+//                 password:"Password"
+//             });
+//         }else{
+//             reject("Error");
+//         }
+//     },1000);
+// });
+
+//consumption
+//5)
+// async function consumePromiseFive(){
+//     try{
+//         const response = await promiseFive;
+        
+//         console.log(response);
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// consumePromiseFive();
